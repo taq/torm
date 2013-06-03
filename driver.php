83c95104f0b3a0a20e82d93732a679cc265095d6
Eustaquio Rangel <taq@eustaquiorangel.com>
2013-06-03 18:00:55 -0300

    One more behaviour for primary key: eval.
    Will be useful when need to eval some expression to use as primary key.

--- driver.php
+++ driver.php
@@ -5,8 +5,10 @@ class Driver {
    const PRIMARY_KEY_DELETE   = 1;
    const PRIMARY_KEY_STRING   = 2;
    const PRIMARY_KEY_EXECUTE  = 3;
-   const LIMIT_APPEND         = 4;
-   const LIMIT_AROUND         = 5;
+   const PRIMARY_KEY_EVAL     = 4;
+
+   const LIMIT_APPEND         = 11;
+   const LIMIT_AROUND         = 12;
 
    public static $primary_key_behaviour = self::PRIMARY_KEY_DELETE;
    public static $limit_behaviour       = self::LIMIT_APPEND;
