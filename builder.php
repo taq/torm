cb55db12e0f735fd9a8493be0280d0fa4a1c3215
Eustaquio Rangel <taq@eustaquiorangel.com>
2015-02-26 10:34:48 -0300

    Builder rewriten to follow PHP code conventions.

--- builder.php
+++ builder.php
@@ -1,56 +1,98 @@
 <?php
+/**
+ * Builder class
+ * Used to build SQL queries
+ *
+ * PHP version 5.5
+ *
+ * @category Associations
+ * @package  TORM
+ * @author   Eustáquio Rangel <taq@bluefish.com.br>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL
+ * @link     http://github.com/taq/torm
+ */
 namespace TORM;
 
-class Builder {
-   public $prefix = "select ";
-   public $fields = null;
-   public $table  = null;
-   public $where  = null;
-   public $limit  = null;
-   public $offset = null;
-   public $order  = null;
+/**
+ * Builder class
+ * Main class
+ *
+ * PHP version 5.5
+ *
+ * @category Associations
+ * @package  TORM
+ * @author   Eustáquio Rangel <taq@bluefish.com.br>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL
+ * @link     http://github.com/taq/torm
+ */
+class Builder
+{
+    public $prefix = "select ";
+    public $fields = null;
+    public $table  = null;
+    public $where  = null;
+    public $limit  = null;
+    public $offset = null;
+    public $order  = null;
 
-   public function toString() {
-      $array  = array();
-      $escape = Driver::$escape_char;
+    /**
+     * Convert to string
+     *
+     * @return SQL query string
+     */
+    public function toString()
+    {
+        $array  = array();
+        $escape = Driver::$escape_char;
 
-      array_push($array,$this->prefix." ");
+        array_push($array, $this->prefix." ");
 
-      if(is_null($this->fields))
-         array_push($array,$escape.$this->table.$escape.".* ");
-      else
-         array_push($array,$this->fields);
+        if (is_null($this->fields)) {
+            array_push($array, $escape.$this->table.$escape.".* ");
+        } else {
+            array_push($array, $this->fields);
+        }
 
-      array_push($array," from $escape".$this->table."$escape ");
+        array_push($array, " from $escape".$this->table."$escape ");
 
-      if($this->where)
-         array_push($array," where ".$this->where);
-      
-      if($this->order)
-         array_push($array," order by ".$this->order);
-      
-      if(!is_null($this->limit) && is_null($this->offset) && Driver::$limit_behaviour==Driver::LIMIT_APPEND)
-         array_push($array," limit ".$this->limit);
+        if ($this->where) {
+            array_push($array, " where ".$this->where);
+        }
 
-      // basic query
-      $query = join(" ",$array);
+        if ($this->order) {
+            array_push($array, " order by ".$this->order);
+        }
 
-      if(!is_null($this->limit) && is_null($this->offset) &&
-         Driver::$limit_behaviour==Driver::LIMIT_AROUND   &&
-         Driver::$limit_query) {
-         $query = str_replace("%query%",$query,Driver::$limit_query);
-         $query = str_replace("%limit%",$this->limit,$query);
-      }
+        if (!is_null($this->limit) && is_null($this->offset) && Driver::$limit_behaviour == Driver::LIMIT_APPEND) {
+            array_push($array, " limit ".$this->limit);
+        }
 
-      if(!is_null($this->limit) && !is_null($this->offset) && Driver::$pagination_query) {
-         $query = str_replace("%query%",$query,Driver::$pagination_query);
-         $query = str_replace("%from%" ,$this->offset,$query);
-         $query = str_replace("%to%"   ,$this->limit ,$query);
-      }
-      return $query;
-   }
+        // basic query
+        $query = join(" ", $array);
 
-   public function __toString() {
-      return $this->toString();
-   }
+        if (!is_null($this->limit) && is_null($this->offset) 
+            && Driver::$limit_behaviour==Driver::LIMIT_AROUND
+            && Driver::$limit_query
+        ) {
+            $query = str_replace("%query%", $query, Driver::$limit_query);
+            $query = str_replace("%limit%", $this->limit, $query);
+        }
+
+        if (!is_null($this->limit) && !is_null($this->offset) && Driver::$pagination_query) {
+            $query = str_replace("%query%", $query, Driver::$pagination_query);
+            $query = str_replace("%from%",  $this->offset, $query);
+            $query = str_replace("%to%",    $this->limit, $query);
+        }
+        return $query;
+    }
+
+    /**
+     * PHP method to convert to string
+     *
+     * @return SQL query string
+     */
+    public function __toString()
+    {
+        return $this->toString();
+    }
 }
