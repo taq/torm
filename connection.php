2b342a639b02fba98e0ac7695c131113065961ca
Eustaquio Rangel <taq@eustaquiorangel.com>
2015-02-27 09:40:15 -0300

    Converted connection class to PHP code conventions.

--- connection.php
+++ connection.php
@@ -1,81 +1,188 @@
 <?php
+/**
+ * Connection class
+ *
+ * PHP version 5.5
+ *
+ * @category Connection
+ * @package  TORM
+ * @author   Eustáquio Rangel <taq@bluefish.com.br>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL
+ * @link     http://github.com/taq/torm
+ */
 namespace TORM;
 
-class Connection {
-   private static $driver = array("development"=> "sqlite",
-                                  "test"       => "sqlite",
-                                  "production" => "sqlite");
+/**
+ * Connection main class
+ *
+ * PHP version 5.5
+ *
+ * @category Connection
+ * @package  TORM
+ * @author   Eustáquio Rangel <taq@bluefish.com.br>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL
+ * @link     http://github.com/taq/torm
+ */
+class Connection
+{
+    private static $_driver = array(
+        "development"=> "sqlite",
+        "test"       => "sqlite",
+        "production" => "sqlite");
 
-   private static $connection = array("development"=> null,
-                                      "test"       => null,
-                                      "production" => null);
+    private static $_connection = array(
+        "development"=> null,
+        "test"       => null,
+        "production" => null);
 
-   private static $encoding = null;
+    private static $_encoding = null;
 
-   public static function setConnection($con,$env=null) {
-      $env = self::selectEnvironment($env);
-      self::$connection[$env] = $con;
+    /**
+     * Set the connection handle
+     *
+     * @param mixed  $con connection handle
+     * @param string $env environment
+     *
+     * @return null
+     */
+    public static function setConnection($con, $env = null) 
+    {
+        $env = self::selectEnvironment($env);
+        self::$_connection[$env] = $con;
 
-      // just send an exception when not on production mode
-      if(in_array($env,array("development","test"))) 
-         self::setErrorHandling(\PDO::ERRMODE_EXCEPTION);
-   }
+        // just send an exception when not on production mode
+        if (in_array($env, array("development","test"))) {
+            self::setErrorHandling(\PDO::ERRMODE_EXCEPTION);
+        }
+    }
 
-   public static function getConnection($env=null) {
-      return self::$connection[self::selectEnvironment($env)];
-   }
+    /**
+     * Return the connection handle
+     *
+     * @param string $env environment
+     *
+     * @return mixed connection handle
+     */
+    public static function getConnection($env = null)
+    {
+        return self::$_connection[self::selectEnvironment($env)];
+    }
 
-   public static function selectEnvironment($env=null) {
-      if(strlen($env)<1) {
-         $getenv = self::getEnvironment();
-         if(strlen($getenv)>0)
-            $env = $getenv;
-         else 
-            $env = "development";
-      }
-      return $env;
-   }
+    /**
+     * Return the current environment
+     *
+     * @param string $env enviroment to be forced
+     *
+     * @return string environment
+     */
+    public static function selectEnvironment($env = null) 
+    {
+        if (strlen($env) < 1) {
+            $getenv = self::_getEnvironment();
+            if (strlen($getenv) > 0) {
+                $env = $getenv;
+            } else {
+                $env = "development";
+            }
+        }
+        return $env;
+    }
 
-   private static function getEnvironment() {
-      return getenv("TORM_ENV");
-   }
+    /**
+     * Return environment from the environment variable
+     *
+     * @return string environment
+     */
+    private static function _getEnvironment()
+    {
+        return getenv("TORM_ENV");
+    }
 
-   public static function setDriver($driver,$env=null) {
-      $file = realpath(dirname(__FILE__)."/drivers/$driver.php");
-      if(!file_exists($file)) {
-         Log::log("ERROR: Driver file $file does not exists");
-         return false;
-      }
-      self::$driver[self::selectEnvironment($env)] = $driver;
-      include $file;
-   }
+    /**
+     * Set the connection database driver
+     *
+     * @param string $driver database driver
+     * @param string $env    environment
+     *
+     * @return driver file
+     */
+    public static function setDriver($driver, $env = null)
+    {
+        $file = realpath(dirname(__FILE__)."/drivers/$driver.php");
+        if (!file_exists($file)) {
+            Log::log("ERROR: Driver file $file does not exists");
+            return null;
+        }
+        self::$_driver[self::selectEnvironment($env)] = $driver;
+        include_once $file;
+    }
 
-   public static function getDriver($env=null) {
-      return self::$driver[self::selectEnvironment($env)];
-   }
+    /**
+     * Return the database driver
+     *
+     * @param string $env environment to check
+     *
+     * @return string driver
+     */
+    public static function getDriver($env = null)
+    {
+        return self::$_driver[self::selectEnvironment($env)];
+    }
 
-   public static function setErrorHandling($strategy) {
-      self::getConnection()->setAttribute(\PDO::ATTR_ERRMODE,$strategy);
-   }
+    /**
+     * Set the error handling strategy
+     *
+     * @param mixed $strategy strategy
+     *
+     * @return null
+     */
+    public static function setErrorHandling($strategy) 
+    {
+        self::getConnection()->setAttribute(\PDO::ATTR_ERRMODE, $strategy);
+    }
 
-   public static function setEncoding($encoding) {
-       self::$encoding = $encoding;
-   }
+    /**
+     * Set encoding
+     *
+     * @param string $encoding encoding
+     *
+     * @return null
+     */
+    public static function setEncoding($encoding)
+    {
+        self::$_encoding = $encoding;
+    }
 
-   public static function getEncoding() {
-       return self::$encoding;
-   }
+    /**
+     * Return encoding
+     *
+     * @return string encoding
+     */
+    public static function getEncoding()
+    {
+        return self::$_encoding;
+    }
 
-   public static function convertToEncoding($mixed) {
-       if (is_null(self::$encoding) ||
-           is_numeric($mixed)       ||
-           is_bool($mixed)          ||
-           is_object($mixed)        || 
-           is_array($mixed)         ||
-          !is_string($mixed)) {
-          return $mixed;  
-       }
-       return mb_convert_encoding($mixed, self::$encoding);
-   }
+    /**
+     * Convert a string to the specified encoding
+     * Paranoid checking
+     *
+     * @param string $mixed string to encode
+     *
+     * @return string encoded
+     */
+    public static function convertToEncoding($mixed)
+    {
+        if (is_null(self::$_encoding)
+            || is_numeric($mixed)
+            || is_bool($mixed)
+            || is_object($mixed)
+            || is_array($mixed)
+            || !is_string($mixed)
+        ) {
+            return $mixed;  
+        }
+        return mb_convert_encoding($mixed, self::$_encoding);
+    }
 }
 ?>
