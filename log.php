085a1d35c9bd2aa9f8d6d94da7be35612ffb7d7a
Eustaquio Rangel <taq@eustaquiorangel.com>
2015-02-27 10:18:35 -0300

    Converted log class to PHP code conventions.

--- log.php
+++ log.php
@@ -1,19 +1,65 @@
 <?php
+/**
+ * Log
+ *
+ * PHP version 5.5
+ *
+ * @category Log
+ * @package  TORM
+ * @author   Eustáquio Rangel <taq@bluefish.com.br>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL
+ * @link     http://github.com/taq/torm
+ */
 namespace TORM;
 
-class Log {
-   private static $enabled = false;
+/**
+ * Log main class
+ *
+ * PHP version 5.5
+ *
+ * @category Log
+ * @package  TORM
+ * @author   Eustáquio Rangel <taq@bluefish.com.br>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL
+ * @link     http://github.com/taq/torm
+ */
+class Log
+{
+    private static $_enabled = false;
 
-   public static function isEnabled() {
-      return self::$enabled;
-   }
+    /**
+     * Return if log is enabled
+     *
+     * @return enabled or not
+     */
+    public static function isEnabled()
+    {
+        return self::$_enabled;
+    }
 
-   public static function enable($status) {
-      self::$enabled = $status;
-   }
+    /**
+     * Enable or disable log
+     *
+     * @param boolean $status enable (true) or disabled (false)
+     *
+     * @return null
+     */
+    public static function enable($status)
+    {
+        self::$_enabled = $status;
+    }
 
-   public static function log($msg) {
-      if(self::$enabled)
-         echo "log: $msg\n";
-   }
+    /**
+     * Send a message to log
+     *
+     * @param string $msg message
+     *
+     * @return null
+     */
+    public static function log($msg)
+    {
+        if (self::$_enabled) {
+            echo "log: $msg\n";
+        }
+    }
 }
