4fd2f25e600b71c9874cef4d27d5fbd96a845f6e
Eustaquio Rangel <taq@eustaquiorangel.com>
2016-09-18 10:32:07 -0300

    Some model tweaks.

--- models/bill.php
+++ models/bill.php
@@ -1,3 +1,30 @@
 <?php
-   class Bill extends TORM\Model {};
+/**
+ * Models
+ *
+ * PHP version 5.5
+ *
+ * @category Tests
+ * @package  TORM
+ * @author   Eustáquio Rangel <taq@bluefish.com.br>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL
+ * @link     http://github.com/taq/torm
+ */
+
+/**
+ * Bill model
+ *
+ * PHP version 5.5
+ *
+ * @category Tests
+ * @package  TORM
+ * @author   Eustáquio Rangel <taq@bluefish.com.br>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL
+ * @link     http://github.com/taq/torm
+ */
+class Bill extends TORM\Model
+{
+}
+
+Bill::setOrder("id");
 ?>
--- models/ticket.php
+++ models/ticket.php
@@ -1,12 +1,41 @@
 <?php
-   class Ticket extends TORM\Model {
-      public static function getNewPKValue() {
-         return time()+rand();
-      }
-   }
+/**
+ * TORM tests
+ *
+ * PHP version 5.5
+ *
+ * @category Tests
+ * @package  TORM
+ * @author   Eustáquio Rangel <taq@bluefish.com.br>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL
+ * @link     http://github.com/taq/torm
+ */
 
-   Ticket::validates("description" ,array("presence"=>true));
-   Ticket::belongsTo("user");
-   Ticket::setOrder("id");
+/**
+ * Ticket model
+ *
+ * PHP version 5.5
+ *
+ * @category Tests
+ * @package  TORM
+ * @author   Eustáquio Rangel <taq@bluefish.com.br>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL
+ * @link     http://github.com/taq/torm
+ */
+class Ticket extends TORM\Model
+{
+    /**
+     * Return new primary key value
+     *
+     * @return int value
+     */
+    public static function getNewPKValue() 
+    {
+        return time() + mt_rand(1, 10000);
+    }
+}
+
+Ticket::validates("description", array("presence" => true));
+Ticket::belongsTo("user");
+Ticket::setOrder("id");
 ?>
-
